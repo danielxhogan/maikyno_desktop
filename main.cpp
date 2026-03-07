bb3967f9975e686d630fa664e6a30f0aaec6edeb
danielxhogan <danielxhogan@gmail.com>
2026-03-06 16:30:03 -0800

    we have a window

--- main.cpp
+++ main.cpp
@@ -1,8 +1,29 @@
 #include <stdio.h>
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QCommandLineParser>
+#include <QDir>
+#include <QUrl>
 
 int main(int argc, char **argv)
 {
-  printf("hi\n");
+  QGuiApplication app(argc, argv);
 
-  return 0;
+  QCoreApplication::setApplicationName("maikyno");
+  QCoreApplication::setOrganizationName("maikyno");
+
+  QCommandLineParser parser;
+  parser.addPositionalArgument("url", "path of media file to play");
+  parser.process(app);
+
+  QQmlApplicationEngine engine;
+  QObject::connect(&engine, &QQmlApplicationEngine::quit, &app, &QGuiApplication::quit);
+
+  QUrl source;
+  if (!parser.positionalArguments().isEmpty())
+    source = QUrl::fromUserInput(parser.positionalArguments().at(0), QDir::currentPath());
+
+  engine.loadFromModule("player", "Main");
+
+  return app.exec();
 }
---
+++ player/Main.qml
@@ -0,0 +1,9 @@
+import QtQuick
+import QtQuick.Controls.Fusion
+
+ApplicationWindow {
+  id: root
+  width: 1280
+  height: 720
+  visible: true
+}
