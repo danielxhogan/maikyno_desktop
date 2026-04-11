6fd70177b2ecaa27df3a7b7f6eb56edbb787e9e3
danielxhogan <danielxhogan@gmail.com>
2026-04-11 16:33:25 -0700

    make seperate build scripts. mkp=experimental/mpv=official

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/zsh
 source ~/.zshrc
 
-mkdir -p ./build_mkp && \
-cd ./build_mkp && \
+mkdir -p ./build_mpv && \
+cd ./build_mpv && \
 cmake \
     -DCMAKE_PREFIX_PATH="/usr/local/bin/6.10.2/gcc_64;$HOME/programs/compiled/install" \
     -DCMAKE_INSTALL_PREFIX="$HOME/programs/compiled/install" \
-    -DUSE_MKP_BACKEND=ON \
     -S .. -B . && \
 cmake --build . && \
-./src/mkd
+cmake --install . && \
+mkd
